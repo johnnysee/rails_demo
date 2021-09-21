9086672709af47d5b1fade064c8623f01875512c
johnnysee <js.johnny.see@gmail.com>
2021-09-21 14:33:31 +0200

    Refactoring code - creating 3 articles is passing

--- spec/requests/api/articles/index_spec.rb
+++ spec/requests/api/articles/index_spec.rb
@@ -1,17 +1,29 @@
-RSpec.describe 'GET /api/articles', type: :request do
+RSpec.describe "GET /api/articles", type: :request do
   subject { response }
-  before do
-    get '/api/articles'
+
+  describe "when there are some articles in the database" do
+    let!(:articles) do
+      create_list(:article, 2)
+      create(:article, title: "my amazing news article")
+    end
+    before do
+      get "/api/articles"
+    end
+
+    it "is expected to return a collection of articles" do
+      expect(response_json["articles"].count).to eq 3
+    end
   end
 
-  describe 'when there are some articles in the database' do
-  end
+  describe "when there are NO articles in the database" do
+    before do
+      get "/api/articles"
+    end
 
-  describe 'when there are NO articles in the database' do
-    it { is_expected.to have_http_status 200}
+    it { is_expected.to have_http_status 200 }
 
-    it 'is expected to respond with a message' do
-      expect(response_json['message']).to eq 'There are no articles'
+    it "is expected to respond with a message" do
+      expect(response_json["message"]).to eq "There are no articles"
     end
   end
 end
